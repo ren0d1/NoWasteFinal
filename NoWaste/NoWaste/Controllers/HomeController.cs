b62bda21d3ef670323131097289aad7ea1ef3a18
Halonico <nicolas.mahieu.isf@gmail.com>
2018-03-10 19:59:44 +0100

    Added : AdvertList, controller call

--- NoWaste/NoWaste/Controllers/HomeController.cs
+++ NoWaste/NoWaste/Controllers/HomeController.cs
@@ -5,14 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NoWaste.Models;
+using NoWaste.Models.HomeViewModels;
+using NoWaste.Repositories;
 
 namespace NoWaste.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UnitOfWork unitOfWork;
+
+        public HomeController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+             List<Advert> l = new List<Advert>();
+
+             for (int i = 0; i < 5; i++)
+             {
+                 Advert a = new Advert();
+                 a.Title = "Test" + i;
+                 a.Description = "AAAAAA AAAAAAA AAAAAA AAAAA AAAAAA AAAAAAA AAAAAAAA AAAAAA AAAAAAAA AAAAAAAAAA AAAAAAAAA";
+                 a.Picture = "http://www.bricotheque-chalon.fr/wp-content/uploads/2016/10/Vélo-rose.png";
+                 l.Add(a);
+             }
+             return View(new AdvertListViewModel()
+             {
+                 List = l
+             });
+            return View(new AdvertListViewModel() { List = adverts });
         }
 
         public IActionResult About()
@@ -38,6 +60,11 @@ namespace NoWaste.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult AdvertDetails(int id)
+        {
+            var advert = unitOfWork.Adverts.GetById(id);
+            return View(advert);
         }
     }
 }
---
+++ NoWaste/NoWaste/Models/HomeViewModels/AdvertListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoWaste.Models.HomeViewModels
+{
+    public class AdvertListViewModel
+    {
+        public List<Advert> List { get; set;  }
+    }
+}

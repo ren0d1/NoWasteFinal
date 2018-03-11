c73e0181fc418fa6e68574f885284973d85a23a2
Quentin <qrasmont@gmail.com>
2018-03-11 10:28:05 +0100

    Add Request to unitOfWork

---
+++ NoWaste/NoWaste/Repositories/RequestRepository.cs
@@ -0,0 +1,15 @@
+﻿using NoWaste.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoWaste.Repositories
+{
+    public class RequestRepository: BaseModelRepository<Request>
+    {
+        public RequestRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
